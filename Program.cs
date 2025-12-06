4b2f096d37a77c5712ae67098490fdb0a6432dab
fmoghaddam <f.bakhshandegan@gmail.com>
2025-12-06 22:58:11 +0100

    init3

--- Program.cs
+++ Program.cs
@@ -5,6 +5,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +27,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Use CORS - must be after UseRouting and before UseAuthorization
+app.UseCors("AllowAll");
+
 app.UseAuthorization();
 app.MapControllers();
 
